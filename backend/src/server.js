2680de8460dd44f21cb7a355daa97595e0bf0589
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-11 15:01:46 +0600

    Testing an Express server with Postman

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 8000;
+app.use(bodyParser.json())
 
 app.get('/hello', (req, res) => {
     res.send('Hello');
+});
+
+app.post('/hello', (req, res) => {
+    res.send(`Hello - ${req.body.name}`);
 });
 
 app.listen(PORT, () => {

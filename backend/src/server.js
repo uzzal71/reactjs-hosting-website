5aaab2864273c9f9784d01828aedc352a3fffedb
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-11 15:20:51 +0600

    Upvoting articles

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
 const app = express();
 const PORT = 8000;
 app.use(bodyParser.json())
 
-app.get('/hello', (req, res) => {
-    res.send('Hello');
-});
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes += 1;
 
-app.get('/hello/:name', (req, res) => {
-    res.send(`Hello - ${req.params.name}`);
-});
-
-app.post('/hello', (req, res) => {
-    res.send(`Hello - ${req.body.name}`);
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
 });
 
 app.listen(PORT, () => {
